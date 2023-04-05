c39d8ce0c49c11d596f0f1ffe2f982eb134a8350
Akhil Rangpariya <akhil.rangpariya@mlveda.com>
2023-04-05 12:36:02 +0530

    add composition example and specialization

---
+++ src/6composition/WelcomeDialog.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import FancyBorder from "./FancyBorder";
+
+function WelcomeDialog() {
+  return (
+    <>
+      {" "}
+      <FancyBorder color="blue">
+        <h1 className="Dialog-title">Welcome</h1>
+      </FancyBorder>
+      <FancyBorder color="red">
+        <p className="Dialog-message">Thank you for visiting our spacecraft!</p>
+      </FancyBorder>
+    </>
+  );
+}
+
+export default WelcomeDialog;
